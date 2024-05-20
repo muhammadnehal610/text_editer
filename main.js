9302475707d37b4285375d7a45c534e0ff664440
muhammadnehal610 <muhammadnehal610@gmail.com>
2024-05-20 07:03:24 +0500

    complet

---
+++ main.js
@@ -0,0 +1,31 @@
+var text = document.getElementById('text')
+var fontSize = document.getElementsByClassName('font_size')
+
+function bolder(){
+text.style.fontWeight = "bold"
+
+}
+function italic(){
+text.style.fontStyle = "italic"
+
+}
+function underline(){
+text.style.textDecoration = "underline"
+
+}
+function allignRight(){
+text.style.textAlign = "end"
+
+}
+function allignLeft(){
+text.style.textAlign = "start"
+
+}
+function justify(){
+text.style.textAlign = "justify"
+
+}
+function center(){
+text.style.textAlign = "center"
+
+}
